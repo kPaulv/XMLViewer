88ede83493993cca9882accb82e3ad66bec594d9
Paul <pavel.k.313@mail.ru>
2021-08-25 18:20:24 +0300

    Add project files.

---
+++ TestTask/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestTask.Models;
+//using TestTask.Models;
+//using Kendo.Mvc.Extensions;
+//using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+
+namespace TestTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Select([DataSourceRequest] DataSourceRequest request)
+        {
+            var data = Enumerable.Range(1, 10)
+                .Select(index => new Product
+                {
+                    ProductID = index,
+                    ProductName = "Product #" + index,
+                    UnitPrice = index * 10,
+                    Discontinued = false
+                });
+
+            return Json(data.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
